7b69c3d7e20dde06712795140e0eac8c0534dba8
Mocca <sirtoastybuns@gmail.com>
2024-06-12 12:44:31 +0700

    inject_translations.py fix

--- Gakumas-Tool/inject_translations.py
+++ Gakumas-Tool/inject_translations.py
@@ -10,6 +10,10 @@ def inject_translations(txt_path, xlsx_path, output_path):
     df['name'] = df['name'].astype(str)
     df['translated name'] = df['translated name'].astype(str)
 
+    # Replace line breaks in the "text" and "translated text" columns with the string "\n"
+    df['text'] = df['text'].apply(lambda x: x.replace('\n', '\\n'))
+    df['translated text'] = df['translated text'].apply(lambda x: x.replace('\n', '\\n'))
+
     # Create a list of tuples with original and translated texts and names
     translation_pairs = list(zip(df['text'], df['translated text'], df['name'], df['translated name']))
 
@@ -25,16 +29,10 @@ def inject_translations(txt_path, xlsx_path, output_path):
                 if f'name={original_name}' in original_line:
                     original_line = original_line.replace(f'name={original_name}', f'name={translated_name}')
 
-            # Inject translated texts sequentially, handling line breaks
+            # Inject translated texts sequentially
             for original_text, translated_text, _, _ in translation_pairs:
                 if f'text={original_text}' in original_line:
-                    if '\n' in translated_text:  # If translated text contains line breaks
-                        translated_text_lines = translated_text.split('\n')
-                        original_line = original_line.replace(f'text={original_text}', f'text={translated_text_lines[0]}')
-                        for additional_line in translated_text_lines[1:]:
-                            file.write(additional_line + '\n')
-                    else:
-                        original_line = original_line.replace(f'text={original_text}', f'text={translated_text}')
+                    original_line = original_line.replace(f'text={original_text}', f'text={translated_text}')
 
             # Ensure that choice texts are not overwritten
             if '[choice' in original_line:
