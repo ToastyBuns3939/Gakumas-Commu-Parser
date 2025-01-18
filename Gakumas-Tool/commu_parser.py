c207d917bd6d2a3590b7afd053db2c267d0805e8
faraplay <faraplay@outlook.com>
2025-01-18 15:55:42 +0000

    use classes in commu parser

--- Gakumas-Tool/commu_parser.py
+++ Gakumas-Tool/commu_parser.py
@@ -1,66 +1,94 @@
 import re
 
-def parse_group_type(text_to_parse):
-    match = re.match(r'([a-z]+)[ \]]', text_to_parse) # Note: re.match searches from the start of the string
-    if not match:
-        raise Exception()
-    return match.group(1), text_to_parse[match.end(1):]
+class ParsingString:
+    def __init__(self, string: str):
+        self.string = string
+
+    def retrieve(self, pattern: re.Pattern[str] | str):
+        # Attempts to match the string with the pattern
+        # Removes the whole match from the string,
+        # and returns the first group of the match
+        match = re.match(pattern, self.string) # Note re.match only matches the start of the string
+        if not match:
+            raise Exception()
+        self.string = self.string[match.end():]
+        return match.group(1)
+    
+    def peek(self, pattern: re.Pattern):
+        match = re.match(pattern, self.string)
+        return bool(match)
+    
+    def is_empty(self):
+        return self.string == ''
+
+
+class CommuGroup:
+    def __init__(self, group_type: str):
+        self.group_type = group_type
+        self.property_pairs = []
+    
+    def append_property(self, key: str, data):
+        self.property_pairs.append((key, data))
+
+    def get_property(self, key: str, defaultValue):
+        found_properties = [pair[1] for pair in self.property_pairs if pair[0] == key]
+        if len(found_properties) == 1:
+            return found_properties[0]
+        elif len(found_properties) == 0:
+            return defaultValue
+        else:
+            raise Exception(f"More than one property with key '{key}' found in group!")
+    
+    def get_property_list(self, key: str):
+        return [pair[1] for pair in self.property_pairs if pair[0] == key]
+    
+    @classmethod
+    def from_commu_line(cls, text_to_parse: str):
+        parsing_string = ParsingString(text_to_parse.strip())
+        group = parse_group(parsing_string)
+        if not parsing_string.is_empty():
+            raise Exception()
+        return group
+
+def parse_group_type(parsing_string: ParsingString):
+    return parsing_string.retrieve(r'([a-z]+)(?=[ \]])')
 
 # This function gobbles up the = as well!
-def parse_key(text_to_parse):
-    match = re.match(r'([A-Za-z]+)=', text_to_parse)
-    if not match:
-        raise Exception()
-    return match.group(1), text_to_parse[match.end():]
+def parse_key(parsing_string: ParsingString):
+    return parsing_string.retrieve(r'([A-Za-z]+)=')
 
-def parse_string_data(text_to_parse):
-    match = re.match(r'((?:[^\n\\\[\]=]|\\n|\\=)+)[ \]]', text_to_parse)
+def parse_string_data(parsing_string: ParsingString):
     # The first match-group consists of any character except
     # linebreak, backslash, square brackets, and equals, unless they are
     # in the combinations '\n' or '\='
-    if not match:
-        raise Exception()
-    data = (match.group(1)
+    return parsing_string.retrieve(r'((?:[^\n\\\[\]=]|\\n|\\=)+)(?=[ \]])')
+
+def unescape_string(string: str):
+    return (string
             .replace('\\n', '\n')
             .replace('\\=', '='))
-    return data, text_to_parse[match.end(1):]
 
-def parse_json_data(text_to_parse):
-    match = re.match(r'(\\\{\S+\\\})([ \]])', text_to_parse)
-    if not match:
-        raise Exception()
-    return match.group(1), text_to_parse[match.end(1):]
+def parse_json_data(parsing_string: ParsingString):
+    return parsing_string.retrieve(r'(\\\{\S+\\\})(?=[ \]])')
 
-def parse_animation_curve_json_data(text_to_parse):
-    match = re.match(r'(AnimationCurve::\\\{\S+\\\})([ \]])', text_to_parse)
-    if not match:
-        raise Exception()
-    return match.group(1), text_to_parse[match.end(1):]
+def parse_animation_curve_json_data(parsing_string: ParsingString):
+    return parsing_string.retrieve(r'(AnimationCurve::\\\{\S+\\\})(?=[ \]])')
 
-def parse_group(text_to_parse):
-    if text_to_parse[0] != '[':
-        raise Exception()
-    group_type, text_to_parse = parse_group_type(text_to_parse[1:])
-    group = {'group_type': group_type, 'property_pairs': []}
-    while text_to_parse[0] != ']':
-        if text_to_parse[0] != ' ':
-            raise Exception()
-        text_to_parse = text_to_parse[1:]
-        key, text_to_parse = parse_key(text_to_parse)
-        if re.match(r'\[', text_to_parse):
-            data, text_to_parse = parse_group(text_to_parse)
-        elif re.match(r'AnimationCurve::\\\{', text_to_parse):
-            data, text_to_parse = parse_animation_curve_json_data(text_to_parse)
-        elif re.match(r'\\\{', text_to_parse):
-            data, text_to_parse = parse_json_data(text_to_parse)
+def parse_group(parsing_string: ParsingString) -> tuple[CommuGroup, str]:
+    parsing_string.retrieve(r'(\[)')
+    group_type = parse_group_type(parsing_string)
+    group = CommuGroup(group_type)
+    while not parsing_string.peek(r'\]'):
+        parsing_string.retrieve(r'( )')
+        key = parse_key(parsing_string)
+        if parsing_string.peek(r'\['):
+            data = parse_group(parsing_string)
+        elif parsing_string.peek(r'AnimationCurve::\\\{'):
+            data = parse_animation_curve_json_data(parsing_string)
+        elif parsing_string.peek(r'\\\{'):
+            data = parse_json_data(parsing_string)
         else:
-            data, text_to_parse = parse_string_data(text_to_parse)
-        group['property_pairs'].append((key, data))
-    return group, text_to_parse[1:]
-
-def parse_commu_line(text_to_parse):
-    text_to_parse = text_to_parse.strip()
-    group, remainder = parse_group(text_to_parse)
-    if remainder != '':
-        raise Exception()
+            data = unescape_string(parse_string_data(parsing_string))
+        group.append_property(key, data)
+    parsing_string.retrieve(r'(\])')
     return group
--- Gakumas-Tool/extract_lines.py
+++ Gakumas-Tool/extract_lines.py
@@ -1,36 +1,26 @@
-from commu_parser import parse_commu_line
+from commu_parser import CommuGroup
 
 # Fields that need translating:
 # message.text, narration.text, choice.text, message.name
 # Fields that might need translating:
 # overwritecharactersetting.name, title.title
 
-def getProperty(group, key, defaultValue):
-    found_properties = [pair[1] for pair in group['property_pairs'] if pair[0] == key]
-    if len(found_properties) == 1:
-        return found_properties[0]
-    elif len(found_properties) == 0:
-        return defaultValue
-    else:
-        raise Exception(f"More than one property with key '{key}' found in group!")
-
-def create_raw_data_rows(group):
-    group_type = group['group_type']
+def create_raw_data_rows(group: CommuGroup):
+    group_type = group.group_type
     
     if group_type == 'choicegroup':
         # A choicegroup contains multiple 'choices' properties
         # We create a data row from each 'choices' property 
         return [
             raw_data_row
-            for subgroup_pair in group['property_pairs']
-                if subgroup_pair[0] == 'choices'
-                    for raw_data_row in create_raw_data_rows(subgroup_pair[1])]
+            for subgroup in group.get_property_list('choices')
+                for raw_data_row in create_raw_data_rows(subgroup)]
     
     if not (group_type == 'message' or group_type == 'narration' or group_type == 'choice'):
         return []
 
-    name = getProperty(group, 'name', '')
-    text = getProperty(group, 'text', '')
+    name = group.get_property('name', '')
+    text = group.get_property('text', '')
     return [(group_type, name, text)]
 
 def extract_lines(file_path):
@@ -38,4 +28,4 @@ def extract_lines(file_path):
         return [
             raw_data_row
             for line in file
-                for raw_data_row in create_raw_data_rows(parse_commu_line(line))]
+                for raw_data_row in create_raw_data_rows(CommuGroup.from_commu_line(line))]
