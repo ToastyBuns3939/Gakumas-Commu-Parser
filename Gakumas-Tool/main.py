bd7c9a28b5f62c6821674bb609a1ed537494d63d
faraplay <faraplay@outlook.com>
2025-01-19 16:44:46 +0000

    separate extraction, injection of one file into a new method

--- Gakumas-Tool/main.py
+++ Gakumas-Tool/main.py
@@ -4,6 +4,7 @@ import time
 import argparse
 import tkinter as tk
 from tkinter import filedialog
+from traceback import TracebackException
 from extract_lines import extract_lines
 from save_to_excel import save_to_excel
 from inject_translations import inject_translations
@@ -43,64 +44,70 @@ def create_argument_parser():
     parser_inject.add_argument(
         "out_txt_directory", help="The directory to hold the modified commu files"
     )
-    parser_inject.set_defaults(func=inject_xlsx_data)
+    parser_inject.set_defaults(func=inject_tl_files)
     return parser
+
+
+def generate_xlsx(input_path, output_path):
+    try:
+        raw_data_rows = extract_lines(input_path)
+        if not raw_data_rows:
+            print(f"No valid lines found in {input_path}. Skipping...")
+        else:
+            save_to_excel(raw_data_rows, output_path, "Sheet1")
+    except Exception as e:
+        print(f"Error generating xlsx for {input_path}:", file=sys.stderr)
+        TracebackException.from_exception(e).print()
+
+
+def inject_tl(txt_path, xlsx_path, output_path):
+    try:
+        inject_translations(txt_path, xlsx_path, output_path)
+        print(f"{txt_path} processed")
+    except FileNotFoundError as e:
+        print(f"File {e.filename} not found, skipping...")
+    except Exception as e:
+        print(f"Error injecting into {txt_path}:", file=sys.stderr)
+        TracebackException.from_exception(e).print()
 
 
 def generate_xlsx_files(args):
     txt_directory = args["txt_directory"]
     xlsx_directory = args["xlsx_directory"]
+    paths = [
+        (
+            os.path.join(txt_directory, file_name),
+            os.path.join(xlsx_directory, os.path.splitext(file_name)[0] + ".xlsx"),
+        )
+        for file_name in os.listdir(txt_directory)
+        if file_name.endswith(".txt")
+    ]
     start_time = time.perf_counter()
-    for file_name in os.listdir(txt_directory):
-        try:
-            if not file_name.endswith(".txt"):
-                continue
-            input_path = os.path.join(txt_directory, file_name)
-            output_path = os.path.join(
-                xlsx_directory, os.path.splitext(file_name)[0] + ".xlsx"
-            )
-
-            raw_data_rows = extract_lines(input_path)
-
-            # Check if there are valid lines for extraction
-            if raw_data_rows:
-                save_to_excel(raw_data_rows, output_path, "Sheet1")
-            else:
-                print(f"No valid lines found in {file_name}. Skipping...")
-        except Exception as e:
-            print(f"Error generating xlsx for {file_name}:")
-            print(e)
-
-    print("Conversion to .xlsx completed.")
+    for path_tuple in paths:
+        generate_xlsx(*path_tuple)
     end_time = time.perf_counter()
+    print("Conversion to .xlsx completed.")
     print(f"Time taken: {end_time - start_time} seconds")
 
 
-def inject_xlsx_data(args):
+def inject_tl_files(args):
     in_txt_directory = args["in_txt_directory"]
     xlsx_directory = args["xlsx_directory"]
     out_txt_directory = args["out_txt_directory"]
+    paths = [
+        (
+            os.path.join(in_txt_directory, file_name),
+            os.path.join(xlsx_directory, os.path.splitext(file_name)[0] + ".xlsx"),
+            os.path.join(out_txt_directory, file_name),
+        )
+        for file_name in os.listdir(in_txt_directory)
+        if file_name.endswith(".txt")
+    ]
     start_time = time.perf_counter()
-    for file_name in os.listdir(in_txt_directory):
-        try:
-            if not file_name.endswith(".txt"):
-                continue
-            txt_path = os.path.join(in_txt_directory, file_name)
-            xlsx_path = os.path.join(
-                xlsx_directory, os.path.splitext(file_name)[0] + ".xlsx"
-            )
-            output_path = os.path.join(out_txt_directory, file_name)
-
-            inject_translations(txt_path, xlsx_path, output_path)
-            print(f"{file_name} processed")
-        except FileNotFoundError as e:
-            print(f"File {e.filename} not found, skipping...")
-        except Exception as e:
-            print(f"Error injecting {file_name}:")
-            print(e)
-
-    print("Translation injection completed.")
+    for path_tuple in paths:
+        inject_tl(*path_tuple)
     end_time = time.perf_counter()
+    print("Translation injection completed.")
     print(f"Time taken: {end_time - start_time} seconds")
 
 
@@ -162,7 +169,7 @@ def main():
                 if not output_folder:
                     raise Exception("No output folder selected.")
 
-                inject_xlsx_data(
+                inject_tl_files(
                     {
                         "in_txt_directory": input_folder,
                         "xlsx_directory": xlsx_folder,
